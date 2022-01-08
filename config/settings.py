73275d981a77a49fb2e0c17bfae2ed841bbc6d34
mahdi zarepour <mahdizarepour15@gmail.com>
2022-01-08 19:04:56 +0330

    add renderer to settings.py

--- config/settings.py
+++ config/settings.py
@@ -143,9 +143,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle',
@@ -158,5 +160,11 @@ REST_FRAMEWORK = {
         # scope
         # 'article_throttle': '1000/day',
         # 'post_throttle': '20/day',
-    }
+    },
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.AdminRenderer',
+    ),
 }
